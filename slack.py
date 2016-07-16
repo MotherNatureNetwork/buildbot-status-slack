01d98ad3c980841005bc2fad35efda3cbb035a4f
Arif Amirani <arif@metroguild.com>
2016-07-16 14:37:06 +0530

    Rewrote everything

--- slack.py
+++ slack.py
@@ -5,57 +5,58 @@ from buildbot import config
 from buildbot.process.results import statusToString
 from buildbot.reporters import utils
 from buildbot.reporters.http import HttpStatusPushBase
+import json
 
+class SlackStatusPush(service.BuildbotService):
+    neededDetails = dict()
 
-class SlackStatusPush(HttpStatusPushBase):
-    name = "SlackStatusPush"
-    neededDetails = dict(wantProperties=True)
-
-    def checkConfig(self, endpoint="https://slack.com", **kwargs):
-        if not isinstance(endpoint, basestring):
-            config.error('endpoint must be a string')
-
-    @defer.inlineCallbacks
-    def reconfigService(self, endpoint="https://slack.com", **kwargs):
-        yield HttpStatusPushBase.reconfigService(self, **kwargs)
-        self.endpoint = endpoint
+    def checkConfig(self, *args, **kwargs):
+        service.BuildbotService.checkConfig(self)
+        if not isinstance(kwargs.get('builders'), (type(None), list)):
+            config.error("builders must be a list or None")
 
     @defer.inlineCallbacks
+    def reconfigService(self, builders=None, **kwargs):
+        yield service.BuildbotService.reconfigService(self)
+        self.builders = builders
+        self.endpoint = kwargs.get('endpoint')
+        for k, v in iteritems(kwargs):
+            if k.startswith("want"):
+                self.neededDetails[k] = v
+
+    @defer.inlineCallbacks
+    def startService(self):
+        yield service.BuildbotService.startService(self)
+
+        startConsuming = self.master.mq.startConsuming
+        self._buildCompleteConsumer = yield startConsuming(
+            self.buildFinished,
+            ('builds', None, 'finished'))
+
+        self._buildStartedConsumer = yield startConsuming(
+            self.buildStarted,
+            ('builds', None, 'new'))
+
+    def stopService(self):
+        self._buildCompleteConsumer.stopConsuming()
+        self._buildStartedConsumer.stopConsuming()
+
     def buildStarted(self, key, build):
-        yield self.send(build, key[2])
+        return self.getMoreInfoAndSend(build)
 
-    @defer.inlineCallbacks
     def buildFinished(self, key, build):
-        yield self.send(build, key[2])
+        return self.getMoreInfoAndSend(build)
+
+    def filterBuilds(self, build):
+        if self.builders is not None:
+            return build['builder']['name'] in self.builders
+        return True
 
     @defer.inlineCallbacks
-    def getBuildDetailsAndSendMessage(self, build, key):
+    def getMoreInfoAndSend(self, build):
         yield utils.getDetailsForBuild(self.master, build, **self.neededDetails)
-        message = yield self.getMessage(build, key)
-        postData = { "text": message }        
-        extra_params = yield self.getExtraParams(build, key)
-        postData.update(extra_params)
-        defer.returnValue(postData)
+        yield self.send(build)
 
-    def getMessage(self, build, event_name):
-        event_messages = {
-            'new': 'Buildbot started build %s here: %s' % (build['builder']['name'], build['url']),
-            'finished': 'Buildbot finished build %s with result %s here: %s'
-                        % (build['builder']['name'], statusToString(build['results']), build['url'])
-        }
-        return event_messages.get(event_name, '')
-
-    # use this as an extension point to inject extra parameters into your postData
-    def getExtraParams(self, build, event_name):
-        return {}
-
-    @defer.inlineCallbacks
-    def send(self, build, key):
-        postData = yield self.getBuildDetailsAndSendMessage(build, key)
-        if not postData or 'text' not in postData or not postData['text']:
-            return
-
-        response = yield self.session.post(url, postData)
-        if response.status_code != 200:
-            log.msg("%s: unable to upload status: %s" %
-                    (response.status_code, response.content))
+    @abc.abstractmethod
+    def send(self, build):
+        requests.post(self.endpoint, data=json.dumps(build))
