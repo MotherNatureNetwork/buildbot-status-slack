bec7782c1d44001edf43e96310ef1f7ad20c7902
Arif Amirani <arif@metroguild.com>
2016-07-17 13:06:13 +0530

    Rewrote again to work with nine

--- slack.py
+++ slack.py
@@ -1,38 +1,95 @@
-import re
+import time
+import warnings
+import json
+from distutils.version import LooseVersion
+import requests
 
+from future.utils import iteritems
 from twisted.internet import defer
+from twisted.internet.protocol import ProcessProtocol
 from twisted.python import log
 
-from buildbot.process.properties import Interpolate
-from buildbot.process.properties import Properties
-from buildbot.process.results import CANCELLED
 from buildbot.process.results import EXCEPTION
 from buildbot.process.results import FAILURE
 from buildbot.process.results import RETRY
-from buildbot.process.results import SKIPPED
 from buildbot.process.results import SUCCESS
 from buildbot.process.results import WARNINGS
-from buildbot.reporters import http
+from buildbot.process.results import SKIPPED
+from buildbot.process.results import CANCELLED
+from buildbot.process.results import Results
+from buildbot.reporters import utils
+from buildbot.util import service
 
 
-class SlackStatusPush(http.HttpStatusPushBase):
+class SlackStatusPush(service.BuildbotService):
     name = "SlackStatusPush"
-    neededDetails = dict(wantProperties=True)
 
-    def checkConfig(self, endpoint, **kwargs):
-        HttpStatusPushBase.checkConfig(self, **kwargs)
-        if not baseUrl:
-            config.error("BaseURL cannot be empty")
-
-    @defer.inlineCallbacks
-    def reconfigService(self, endpoint=None, **kwargs):
-        yield http.HttpStatusPushBase.reconfigService(self, **kwargs)
+    def reconfigService(self, endpoint=None):
         self.endpoint = endpoint
 
     @defer.inlineCallbacks
-    def send(self, build):
-        response = yield self.session.post(self.endpoint, json=build)
-        if response.status_code != 200:
-            log.msg("%s: unable to upload status: %s" %
-                    (response.status_code, response.content))
-            
+    def startService(self):
+        yield service.BuildbotService.startService(self)
+        startConsuming = self.master.mq.startConsuming
+        self._buildCompleteConsumer = yield startConsuming(
+            self.buildStarted,
+            ('builds', None, 'finished'))
+
+        self._buildStartedConsumer = yield startConsuming(
+            self.buildStarted,
+            ('builds', None, 'new'))
+
+    def stopService(self):
+        self._buildCompleteConsumer.stopConsuming()
+        self._buildStartedConsumer.stopConsuming()
+
+    @defer.inlineCallbacks
+    def buildStarted(self, key, build):
+        yield self.getBuildDetails(build)
+
+        msg, color = self.createStatusString(build)
+        payload = {
+            "username": "buildbot",
+            "attachments": [
+                {
+                    "color": color,
+                    "text": msg,
+                    "mrkdwn_in": ["text"]
+                }
+            ]
+        }
+
+        log.msg(build)
+        yield requests.post(self.endpoint, json=payload)
+
+    @defer.inlineCallbacks
+    def buildComplete(self, key, build):
+        yield self.getBuildDetails(build)
+
+    @defer.inlineCallbacks
+    def getBuildDetails(self, build):
+        br = yield self.master.data.get(("buildrequests", build['buildrequestid']))
+        buildset = yield self.master.data.get(("buildsets", br['buildsetid']))
+        yield utils.getDetailsForBuilds(self.master, buildset, [build], wantProperties=True)
+
+    def createStatusString(self, build):
+        state = ('unknown', 'good')
+        if build['complete']:
+            state = {
+                SUCCESS: ('success', 'good'),
+                WARNINGS: ('success', 'good'),
+                FAILURE: ('failure', 'danger'),
+                SKIPPED: ('success', 'good'),
+                EXCEPTION: ('error', 'danger'),
+                RETRY: ('pending', 'warning'),
+                CANCELLED: ('error', 'danger')
+            }.get(build['results'], ('error', 'danger'))
+
+        build_date = build['started_at'].strftime('%x %X')
+        if build['complete']:
+            build_date = build['complete_at'].strftime('%x %X')
+
+        msg = '{} {} build for {} on {}. Reason: {}.'.format(build['properties']['owner'][0], build['state_string'], build['builder']['name'], build_date, build['properties']['reason'][0])
+        if build['complete']:
+            msg += ' Build ended in {}'.format(state[0])
+        return msg, state[1]
