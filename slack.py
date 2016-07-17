cb6d08d8f4eae3eb0cf3c2543444d2281c6126c5
Arif Amirani <arif@metroguild.com>
2016-07-17 11:18:35 +0530

    Over simplified

--- slack.py
+++ slack.py
@@ -1,62 +1,38 @@
+import re
+
 from twisted.internet import defer
 from twisted.python import log
 
-from buildbot import config
-from buildbot.process.results import statusToString
-from buildbot.reporters import utils
-from buildbot.reporters.http import HttpStatusPushBase
-import json
+from buildbot.process.properties import Interpolate
+from buildbot.process.properties import Properties
+from buildbot.process.results import CANCELLED
+from buildbot.process.results import EXCEPTION
+from buildbot.process.results import FAILURE
+from buildbot.process.results import RETRY
+from buildbot.process.results import SKIPPED
+from buildbot.process.results import SUCCESS
+from buildbot.process.results import WARNINGS
+from buildbot.reporters import http
 
-class SlackStatusPush(service.BuildbotService):
-    neededDetails = dict()
 
-    def checkConfig(self, *args, **kwargs):
-        service.BuildbotService.checkConfig(self)
-        if not isinstance(kwargs.get('builders'), (type(None), list)):
-            config.error("builders must be a list or None")
+class SlackStatusPush(http.HttpStatusPushBase):
+    name = "SlackStatusPush"
+    neededDetails = dict(wantProperties=True)
 
-    @defer.inlineCallbacks
-    def reconfigService(self, builders=None, **kwargs):
-        yield service.BuildbotService.reconfigService(self)
-        self.builders = builders
-        self.endpoint = kwargs.get('endpoint')
-        for k, v in iteritems(kwargs):
-            if k.startswith("want"):
-                self.neededDetails[k] = v
+    def checkConfig(self, endpoint, **kwargs):
+        HttpStatusPushBase.checkConfig(self, **kwargs)
+        if not baseUrl:
+            config.error("BaseURL cannot be empty")
 
     @defer.inlineCallbacks
-    def startService(self):
-        yield service.BuildbotService.startService(self)
-
-        startConsuming = self.master.mq.startConsuming
-        self._buildCompleteConsumer = yield startConsuming(
-            self.buildFinished,
-            ('builds', None, 'finished'))
-
-        self._buildStartedConsumer = yield startConsuming(
-            self.buildStarted,
-            ('builds', None, 'new'))
-
-    def stopService(self):
-        self._buildCompleteConsumer.stopConsuming()
-        self._buildStartedConsumer.stopConsuming()
-
-    def buildStarted(self, key, build):
-        return self.getMoreInfoAndSend(build)
-
-    def buildFinished(self, key, build):
-        return self.getMoreInfoAndSend(build)
-
-    def filterBuilds(self, build):
-        if self.builders is not None:
-            return build['builder']['name'] in self.builders
-        return True
+    def reconfigService(self, endpoint=None, **kwargs):
+        yield http.HttpStatusPushBase.reconfigService(self, **kwargs)
+        self.endpoint = endpoint
 
     @defer.inlineCallbacks
-    def getMoreInfoAndSend(self, build):
-        yield utils.getDetailsForBuild(self.master, build, **self.neededDetails)
-        yield self.send(build)
-
-    @abc.abstractmethod
     def send(self, build):
-        requests.post(self.endpoint, data=json.dumps(build))
+        response = yield self.session.post(self.endpoint, json=build)
+        if response.status_code != 200:
+            log.msg("%s: unable to upload status: %s" %
+                    (response.status_code, response.content))
+            
